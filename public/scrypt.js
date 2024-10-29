eb694f5e9fffb6ed37cac45d942552e4ffc3a292
bouchta mohamed <bouchtamohamed01@gmail.com>
2024-10-29 12:59:48 +0100

    Set up Tailwind CSS project structure

---
+++ public/scrypt.js
@@ -0,0 +1,16 @@
+document.querySelector("#btn-nav").addEventListener('click',function(){
+    const navbarCta = document.getElementById('navbar-cta')
+    navbarCta.classList.toggle('hidden');
+
+});
+
+document.querySelector("#btn-modal").addEventListener('click',function(){
+    const crudmodal = document.getElementById('crud-modal')
+    crudmodal.classList.toggle('hidden') 
+
+})
+
+document.querySelector("#btn-close").addEventListener('click',function(){
+    const crudmodal = document.getElementById('crud-modal')
+    crudmodal.classList.toggle('hidden')
+})
