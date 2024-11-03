b71908f79ccffee817c36c04e126fd76b8095407
bouchta mohamed <bouchtamohamed01@gmail.com>
2024-11-03 19:18:26 +0100

    Ajout de scripts pour intégrer l'icône de burger dans la navigation et le champ de recherche.

--- public/scryptAll.js
+++ public/scryptAll.js
@@ -1,8 +1,22 @@
+
 document.querySelector("#btn-nav").addEventListener('click',function(){
     const navbarCta = document.getElementById('navbar-cta')
     navbarCta.classList.toggle('hidden');
 
 });
+
+document.querySelector("#button-nav").addEventListener('click',function(){
+    const sidebar = document.getElementById('default-sidebar')
+    sidebar.classList.toggle('show');
+});
+
+document.querySelector("#close-slidbar").addEventListener('click',function(){
+    const sidebar = document.getElementById('default-sidebar')
+    sidebar.classList.toggle('show');
+});
+
+
+
 
 
 
